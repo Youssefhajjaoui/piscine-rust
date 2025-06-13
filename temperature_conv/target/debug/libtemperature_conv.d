0fbb08d8f5ee11861b531e063029c5444049faa3
yhajjaou <youssefhajjaoui67@gmail.com>
2025-06-13 15:46:19 +0100

    temperature

--- temperature_conv/target/debug/libtemperature_conv.d
+++
@@ -1,1 +0,0 @@
-/home/yhajjaou/piscine-rust/temperature_conv/target/debug/libtemperature_conv.rlib: /home/yhajjaou/piscine-rust/temperature_conv/src/lib.rs
--- temperature_conv/target/debug/temperature_conv.d
+++
@@ -1,1 +0,0 @@
-/home/yhajjaou/piscine-rust/temperature_conv/target/debug/temperature_conv: /home/yhajjaou/piscine-rust/temperature_conv/src/lib.rs /home/yhajjaou/piscine-rust/temperature_conv/src/main.rs
