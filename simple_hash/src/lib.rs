71d863b93f233d1d630d4f28546923898560d535
yhajjaou <youssefhajjaoui67@gmail.com>
2025-06-15 17:49:02 +0100

    simplehash

---
+++ simple_hash/src/lib.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut res = HashMap::new();
+
+    for &word in words {
+        *res.entry(word).or_insert(0) += 1;
+    }
+
+    res
+}
+
+
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.keys().len()
+}
