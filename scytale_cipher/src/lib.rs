167bbefa4b6811224b7698db7afc8d78f36fc1ad
yhajjaou <youssefhajjaoui67@gmail.com>
2025-06-22 02:53:32 +0100

    scytale cipher

---
+++ scytale_cipher/src/lib.rs
@@ -0,0 +1,31 @@
+pub fn scytale_cipher(message: String, i: u32) -> String {
+    let mut name = message.to_string();
+    let res: Option<String> = None;
+    if i == name.len() as u32 {
+        return message;
+    }
+    let mut result = String::new();
+    let mut res: Vec<Vec<char>> = Vec::new();
+    let mut j = 0;
+    while j < message.len() {
+        let mut first: Vec<char> = vec![' '; i as usize];
+        let mut newname = String::new();
+        for (index, chare) in name.chars().enumerate() {
+            j += 1;
+            first[index] = chare;
+            newname = name[index + 1..].to_string();
+            if index + 1 == i as usize {
+                break;
+            }
+        }
+        name = newname;
+        res.push(first);
+    }
+
+    for j in 0..i as usize {
+        for arr in res.clone() {
+            result.push(arr[j]);
+        }
+    }
+    return result.trim().to_string();
+}
